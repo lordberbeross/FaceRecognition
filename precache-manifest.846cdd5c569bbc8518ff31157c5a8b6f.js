9fd525d74d9b48f5715c7a49fb115cb0dfa205ec
LordBerbeross <mustafaberber020@gmail.com>
2019-04-03 01:15:59 +0300

    Updates

---
+++ precache-manifest.846cdd5c569bbc8518ff31157c5a8b6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c762eb56584dcbc05797",
+    "url": "/FaceRecognition/static/js/runtime~main.4997f496.js"
+  },
+  {
+    "revision": "8de635925f1c67021d75",
+    "url": "/FaceRecognition/static/js/main.8542f581.chunk.js"
+  },
+  {
+    "revision": "68da4efbf3123870ff45",
+    "url": "/FaceRecognition/static/js/2.74f2ac0f.chunk.js"
+  },
+  {
+    "revision": "8de635925f1c67021d75",
+    "url": "/FaceRecognition/static/css/main.02370887.chunk.css"
+  },
+  {
+    "revision": "68da4efbf3123870ff45",
+    "url": "/FaceRecognition/static/css/2.b6349f32.chunk.css"
+  },
+  {
+    "revision": "727ae93012d1a58547d8c61d110c929f",
+    "url": "/FaceRecognition/index.html"
+  }
+];
